b7c8be4d30450fae023028e312be06d02db36d4c
tamara24github <tamara24github@gmail.com>
2024-01-13 14:23:47 +0100

    feat: comments removed, updated footer and heading added to shop page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import Paragraph from './common/Paragraph'
 
 function Footer() {
   return (
-    <div className="flex justify-center w-full mb-2">
+    <footer className="flex justify-center w-full mb-2">
       <Paragraph size="md" color="black">
         © 2023 John Doe All rights reserved.
       </Paragraph>
-    </div>
+    </footer>
   )
 }
 
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,6 +1,7 @@
 import { getAllProducts } from '../services/products'
 import { useQuery } from '@tanstack/react-query'
 import ShopItem from '../components/ShopItem'
+import Heading from '../components/common/Heading'
 
 function Shop() {
   const productsQuery = useQuery({
@@ -22,15 +23,15 @@ function Shop() {
 
   return (
     <>
-      <form className="mx-auto my-[3rem] text-center">
-        <label className="font-semibold text-2xl text-blue-950">
+      <div className="mx-auto my-[3rem] text-center">
+        <Heading fontWeight="semibold" fontSize="xl2" color="blueDark">
           Explore our Shop: Elevate your ride
-        </label>
+        </Heading>
         <input
           className="mt-3 block md:w-[600px] w-full px-3 py-2 border-2 border-blue-400 rounded-md focus:outline-none focus:border-blue-950 focus:ring-1 focus:ring-blue-400"
           placeholder="Search"
         />
-      </form>
+      </div>
       <ul className="grid lg:grid-cols-4 md:grid-cols-2 gap-10 mx-[4rem] my-[2rem] ">
         {data?.map((product) => {
           return <ShopItem product={product} key={product.id} />
