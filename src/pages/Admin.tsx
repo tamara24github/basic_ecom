e99092664f9824b9c6f1356e0a0c685a34667e59
tamara24github <133815770+tamara24github@users.noreply.github.com>
2024-03-19 18:04:36 +0100

    feat: added filter to a search bar (#11)
    * feat: added filter to a search bar
    
    * fix: order of elements changed

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,14 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
 import Table from '../components/common/Table'
-import { getAllProducts } from '../services/products'
+import { Product, getAllProducts } from '../services/products'
 import TextField from '../components/common/TextField'
 import Button from '../components/common/Button'
 import { IoMdAddCircleOutline } from 'react-icons/io'
+import { ChangeEvent, useCallback, useState } from 'react'
 
 function Admin() {
+  const [searchItem, setSearchItem] = useState('')
+
+  const handleSearchItem = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchItem(e.target.value)
+  }
+
+  const productsFilter = useCallback(
+    (products: Product[]) =>
+      products.filter((product) =>
+        product.name.toLowerCase().includes(searchItem.toLowerCase()),
+      ),
+    [searchItem],
+  )
+
   const productsQuery = useQuery({
     queryKey: ['products'],
     queryFn: () => getAllProducts(),
+    select: productsFilter,
   })
 
   const { error, isLoading, data } = productsQuery
@@ -16,17 +32,22 @@ function Admin() {
   return (
     <div className="flex flex-col items-center">
       <div className="flex my-12">
-        <TextField placeholder="Search" className="md:w-[500px]" />
         <Button
           backgroundColor="blueLight"
           hover="blueDark"
           rounded="xl2"
           fontWeight="bold"
-          className="ml-6 px-6 py-2 text-lg flex items-center"
+          className="mr-16  px-6 py-2 text-lg flex items-center"
         >
           <IoMdAddCircleOutline className="mr-2 w-[24px] h-[24px]" />
-          Add
+          Add Product
         </Button>
+        <TextField
+          placeholder="Search Product"
+          className="md:w-[500px]"
+          onChange={handleSearchItem}
+          value={searchItem}
+        />
       </div>
 
       <Table data={data || []} isLoading={isLoading} error={error}></Table>
