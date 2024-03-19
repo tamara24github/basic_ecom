bfcb5348ad57da92761ae57f68de09fdd106c809
tamara24github <tamara24github@gmail.com>
2024-03-19 18:40:50 +0100

    feat: modal added

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -5,9 +5,19 @@ import TextField from '../components/common/TextField'
 import Button from '../components/common/Button'
 import { IoMdAddCircleOutline } from 'react-icons/io'
 import { ChangeEvent, useCallback, useState } from 'react'
+import Modal from '../components/common/Modal'
+import Paragraph from '../components/common/Paragraph'
 
 function Admin() {
+  const [showModal, setShowModal] = useState(false)
   const [searchItem, setSearchItem] = useState('')
+
+  const handleOpenModal = () => {
+    setShowModal(true)
+  }
+  const handleCloseModal = () => {
+    setShowModal(false)
+  }
 
   const handleSearchItem = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchItem(e.target.value)
@@ -29,29 +39,67 @@ function Admin() {
 
   const { error, isLoading, data } = productsQuery
 
-  return (
-    <div className="flex flex-col items-center">
-      <div className="flex my-12">
-        <Button
-          backgroundColor="blueLight"
-          hover="blueDark"
-          rounded="xl2"
-          fontWeight="bold"
-          className="mr-16  px-6 py-2 text-lg flex items-center"
-        >
-          <IoMdAddCircleOutline className="mr-2 w-[24px] h-[24px]" />
-          Add Product
-        </Button>
-        <TextField
-          placeholder="Search Product"
-          className="md:w-[500px]"
-          onChange={handleSearchItem}
-          value={searchItem}
-        />
-      </div>
+  const actionBar = (
+    <>
+      <Button
+        backgroundColor="blueDark"
+        hover="blueLight"
+        fontWeight="bold"
+        className="mr-10 p-2 w-28 text-lg "
+      >
+        Add
+      </Button>
+      <Button
+        fontWeight="bold"
+        backgroundColor="transparent"
+        textColor="red"
+        hover="red"
+        onClick={handleCloseModal}
+        className="p-2 w-28 text-lg border  border-red-600"
+      >
+        Close
+      </Button>
+    </>
+  )
 
-      <Table data={data || []} isLoading={isLoading} error={error}></Table>
-    </div>
+  return (
+    <>
+      {showModal && (
+        <Modal closeModal={handleCloseModal} actionBar={actionBar}>
+          <Paragraph
+            weight="semibold"
+            color="blueDark"
+            className="mx-auto text-xl"
+          >
+            Add Product
+          </Paragraph>
+        </Modal>
+      )}
+      <div className="flex flex-col items-center">
+        <div className="flex my-12">
+          <Button
+            backgroundColor="blueLight"
+            hover="blueDark"
+            rounded="xl2"
+            fontWeight="bold"
+            className="mr-16  px-6 py-2 text-lg flex items-center"
+            onClick={handleOpenModal}
+          >
+            <IoMdAddCircleOutline className="mr-2 w-[24px] h-[24px]" />
+            Add Product
+          </Button>
+
+          <TextField
+            placeholder="Search Product"
+            className="md:w-[500px]"
+            onChange={handleSearchItem}
+            value={searchItem}
+          />
+        </div>
+
+        <Table data={data || []} isLoading={isLoading} error={error}></Table>
+      </div>
+    </>
   )
 }
 
