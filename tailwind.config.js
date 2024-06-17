27949330edefb58d2f059f0cb5e18c8c1cb925ab
tamara24github <tamara24github@gmail.com>
2024-06-17 21:27:59 +0200

    feat: added toasts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        toast: 'toast 5s cubic-bezier(0.16, 1, 0.3, 1) forwards',
+      },
+      keyframes: {
+        toast: {
+          '0%, 100%': {
+            transform: 'translateY(calc(-100% - 24px))',
+            opacity: 0,
+          },
+          '10%, 90%': { transform: 'translateY(0)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
