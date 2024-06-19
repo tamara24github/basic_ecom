453fbb3c1064352d7175e6b932362877cdd72efc
tamara24github <133815770+tamara24github@users.noreply.github.com>
2024-06-19 19:20:46 +0200

    feat: added toast icons (#22)

--- src/contexts/ToastContext.tsx
+++ src/contexts/ToastContext.tsx
@@ -1,6 +1,10 @@
 import { nanoid } from 'nanoid'
 import { createContext, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
+import {
+  IoIosCheckmarkCircleOutline,
+  IoIosCloseCircleOutline,
+} from 'react-icons/io'
 
 type ToastContextType = {
   toast: ({ message, type }: { message: string; type: Toast['type'] }) => void
@@ -48,7 +52,7 @@ export const ToastContextProvider = ({
             <li
               key={toast.id}
               className={twMerge(
-                'animate-toast rounded-2xl border-2 py-2 px-4 shadow-md',
+                'animate-toast rounded-2xl border-2 py-2 px-4 shadow-md flex flex-row items-center font-semibold',
                 toast.type === 'success' &&
                   ' border-green-400 bg-green-100 text-green-950 ',
                 toast.type === 'error' &&
@@ -56,6 +60,12 @@ export const ToastContextProvider = ({
               )}
               onAnimationEnd={() => deleteToastById(toast.id)}
             >
+              {toast.type === 'success' && (
+                <IoIosCheckmarkCircleOutline className="mr-2 text-2xl text-green-600 " />
+              )}
+              {toast.type === 'error' && (
+                <IoIosCloseCircleOutline className="mr-2 text-2xl text-red-600 " />
+              )}
               {toast.message}
             </li>
           )
